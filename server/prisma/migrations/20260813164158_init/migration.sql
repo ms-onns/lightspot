33eb8c6917340d91febbabd0fd57fd2d4585b84c
Max Sheyn <nmmn1905@gmail.com>
2026-08-13 19:53:31 +0300

    feat: initialize backend with prisma, postgresql and spot schema

---
+++ server/prisma/migrations/20260813164158_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Spot" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "lat" DOUBLE PRECISION NOT NULL,
+    "lng" DOUBLE PRECISION NOT NULL,
+    "hasLight" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Spot_pkey" PRIMARY KEY ("id")
+);
